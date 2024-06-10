4f5b3b530c6d1c25387c63b5a1c90911403ec56a
mityaaa <burydima28122003@gmail.com>
2024-06-10 14:19:42 +0300

    chore: clear basic "Hello World"
    Delete start styles, components, icons and etc.

--- users-table/src/views/HomeView.vue
+++ users-table/src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <h1>Hello World!</h1>
+      <p>Description</p>
+    </main>
 </template>
